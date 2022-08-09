65ec8621032c2d98311eccc348812891fb1d5159
cathome <cathome.bro@yandex.ru>
2022-08-09 21:52:11 +0300

    fourth task finished

---
+++ task_8/Program.cs
@@ -0,0 +1,13 @@
+﻿//  Напишите программу, которая на вход принимает число (N), 
+//  а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i < a)
+{
+    string res = (i % 2) == 0 ? i.ToString():" ";
+    Console.Write(res);
+    i = i + 1;
+}
